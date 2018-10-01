7035cbc20ce5dafbc2e1cf9ffafb7ca2b0ff3126
Halit Ipek <halitipek89@gmail.com>
2018-10-02 00:28:20 +0200

    fill the area

--- src/components/List/ForecastChart.js
+++ src/components/List/ForecastChart.js
@@ -1,63 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactEcharts from 'echarts-for-react'
 
-const ForecastChart = (props) => {
-  const option = {
-    title: {
-      text: props.title || ''
-    },
-    legend: {
-      show: false
-    },
-    xAxis: {
-      type: 'category',
-      data: props.data.breakpoints,
-      axisTick: {
-        alignWithLabel: true
-      },
-      splitLine : {
-          show:true,
-          interval : function(param) {
-              return param % 1 === 0;
-          }
-      }
-    },
-    yAxis: {
-      type: 'value',
-      name: '℃',
-      scale: true
-    },
-    grid: {
-      left: '1%',
-      right: '3%',
-      bottom: '15%',
-      containLabel: true
-    },
-    dataZoom: [
-      {
-          type: 'slider',
-          show: true,
-          xAxisIndex: [0],
-          start: 0,
-          end: props.data.focusEnd
-      },
-      {
-          type: 'inside',
-          xAxisIndex: [0],
-          start: 0,
-          end: props.data.focusEnd
-      },
-    ],
-    series: props.data.series
+class ForecastChart extends Component {
+  shouldComponentUpdate(nextProps) {
+    return JSON.stringify(this.props.data) !== JSON.stringify(nextProps.data)
   }
 
-  return (
-    <ReactEcharts
-      option={option}
-      style={{height: '350px', width: '100%'}}
-      className='react_for_echarts' />
-  )
+  getOption = () => {
+    const { props } = this
+
+    return {
+      title: {
+        text: props.title || ''
+      },
+      legend: {
+        show: false
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            animation: false
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: props.data.breakpoints,
+        axisTick: {
+          alignWithLabel: true
+        },
+        splitLine : {
+            show:false,
+            interval : function(param) {
+                return param % 1 === 0;
+            }
+        }
+      },
+      yAxis: {
+        type: 'value',
+        name: '℃',
+        scale: true,
+        splitLine: {
+          show: false
+        }
+      },
+      grid: {
+        left: '1%',
+        right: '3%',
+        bottom: '15%',
+        containLabel: true
+      },
+      dataZoom: [
+        {
+            type: 'slider',
+            show: true,
+            xAxisIndex: [0],
+            start: 0,
+            end: props.data.focusEnd
+        },
+        {
+            type: 'inside',
+            xAxisIndex: [0],
+            start: 0,
+            end: props.data.focusEnd
+        },
+      ],
+      series: props.data.series
+    }
+  }
+
+  render() {
+    return (
+      <ReactEcharts
+        option={this.getOption()}
+        style={{height: '350px', width: '100%'}}
+        className='react_for_echarts' />
+    )
+  }
 }
 
 ForecastChart.propTypes = {
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -12,7 +12,14 @@ import List from '../../components/List'
 class App extends Component {
   state = {
     keyword: '',
-    loading: false
+    loading: false,
+    ranges: {
+      ice: [-200, 4, 'blue'],
+      cold: [4, 12, 'cyan'],
+      mid: [12, 18, 'brown'],
+      hot: [15, 25, 'yellow'],
+      sun: [25, 200, 'red']
+    }
   }
 
   handleChange = (e) => {
@@ -28,6 +35,8 @@ class App extends Component {
   }
 
   convertDataForList = ({ currentWeather, weatherForecast }) => {
+    const { ranges } = this.state
+
     const list = weatherForecast.list
       .sort((a, b) => a.dt - b.dt)
       .map(period => ({
@@ -36,20 +45,20 @@ class App extends Component {
         time: moment(period.dt * 1000).format('HH:mm'),
         day: moment(period.dt * 1000).format('dddd')
       }))
-      
+
     return {
       currentWeather: {
         title: currentWeather.name,
         desc: currentWeather.weather[0].description,
         icon: 'http://openweathermap.org/img/w/' + currentWeather.weather[0].icon + '.png',
-        temp: currentWeather.main.temp,
+        temp: currentWeather.main.temp.toFixed(1),
         time: moment(currentWeather.dt * 1000).calendar()
       },
 
       weatherForecast: {
         focusEnd: Math.floor((list.reduce((r, v, i, a, k = v.day) => ((r[k] || (r[k] = [])).push(v), r), {})[list[0].day].length) / (list.length) * 100) - 1,
         breakpoints: list.map(period => `${period.time}\n${period.day}`),
-        series: list.reduce((acc, cur, i) => {
+        series: list.reduce((acc, cur, i, arr) => {
           if (i === 0) {
             acc[0] = {
               name: 'Average',
@@ -57,11 +66,26 @@ class App extends Component {
               symbolSize: 30,
               symbolOffset: [0, '-20%'],
               smooth: true,
-              data: []
+              data: [],
+              areaStyle: {
+                color: {
+                  type: 'linear',
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [{
+                      offset: 0, color: ranges[Object.keys(ranges).filter(key => ranges[key][0] <= Math.max(...arr.map(elem => elem.temp)) && ranges[key][1] > Math.max(...arr.map(elem => elem.temp)))[0]][2]
+                  },
+                  {
+                      offset: 1, color: ranges[Object.keys(ranges).filter(key => ranges[key][0] <= Math.min(...arr.map(elem => elem.temp)) && ranges[key][1] > Math.min(...arr.map(elem => elem.temp)))[0]][2]
+                  }]
+                }
+              }
             }
           }
 
-          acc[0].data.push({ value: cur.temp, symbol: `image://${cur.icon}` })
+          acc[0].data.push({ value: cur.temp.toFixed(1), symbol: `image://${cur.icon}` })
 
           return acc
         }, [])
